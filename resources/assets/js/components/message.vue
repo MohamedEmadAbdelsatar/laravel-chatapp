1f352dc84739fcca9829c2c4438643917c668ba1
MohamedEmadAbdelsatar <medo_emad2011@yahoo.com>
2018-08-12 05:57:52 +0200

    new laravel project

---
+++ resources/assets/js/components/message.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <li class="list-group-item " :class="className"><span class="badge badge-primary float-rigth">{{user}}:</span><slot></slot><small style="float: right;">{{time}}</small></li>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[
+            'color','user','time'
+        ],
+        computed:{
+            className(){
+                return 'list-group-item-'+this.color
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
